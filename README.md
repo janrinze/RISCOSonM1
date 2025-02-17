ab8742b9cc1286fadfa177fd2c844558a05d4d10
janrinze <janrinze@M1DebianSid.local>
2025-02-17 19:59:28 +0100

    Add files.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # RISCOSonM1
 Patches and tools to setup RISC OS under Linux on platforms like Apple M1.
+
+Install Debian 12 in UTM for Apple.
+
+On Linux install the dependancies:
+
+```
+sudo apt install attr bash bubblewrap build-essential g++ gcc git libattr1 libattr1-dev libsdl2-dev libseccomp-dev libseccomp2 make ninja-build pkg-config
+```
+
+Install this repository:
+```
+git clone git@github.com:janrinze/RISCOSonM1.git
+```
+
+Run the installer:
+
+```
+cd RISCOSonM1
+./install_M1.sh
+```
---
+++ install_M1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git clone https://github.com/TimothyEBaldwin/RISC_OS_Linux_Binary.git
+
+cd RISC_OS_Linux_Binary
+patch -p1 < ../M1.patch
+make -j8 Built/qemu-arm
+make -j8 opengl
+
+RISCOSIMG=./Unix/RISCOS.IMG
+
+mkdir -p ./hostfs
+cp ../HardDisc4.util ./hostfs/HardDisc4.util,ffc
+RISC_OS_IXFS_HardDisc4='IXFS:$' RISC_OS_Alias_IXFSBoot="run HardDisc4/util" ./opengl bwrap --bind ./hostfs / --ro-bind /usr /usr --ro-bind /lib /lib --ro-bind ./Built/qemu-arm /qemu-arm --ro-bind $RISCOSIMG /RISCOS.IMG --proc /proc --chdir / ./qemu-arm RISCOS.IMG --abort-on-input
+
+echo >> hostfs/HardDisc4/\!Boot/RO520Hook/Boot/Desktop
+echo "AddTinyDir HardDisc4" >> hostfs/HardDisc4/\!Boot/RO520Hook/Boot/Desktop
+
+# boot RISC OS
+RISC_OS_IXFS_HardDisc4='IXFS:$.HardDisc4' RISC_OS_Alias_IXFSBoot='HardDisc4.!Boot' ./opengl bwrap --bind ./hostfs / --ro-bind /usr /usr --ro-bind /lib /lib --ro-bind ./Built/qemu-arm /qemu-arm --ro-bind $RISCOSIMG /RISCOS.IMG --proc /proc --chdir / ./qemu-arm RISCOS.IMG >/dev/null
