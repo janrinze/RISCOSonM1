1dfc25bb12d49100b76d80e416d1b5d2d18476a3
janrinze <janrinze@M1DebianSid.local>
2025-02-17 20:28:11 +0100

    Allow scripts to be run many times.

--- install_M1.sh
+++ install_M1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+RISCOSIMG=./Unix/RISCOS.IMG
+if [[ ! -d RISC_OS_Linux_Binary ]] then
 git clone https://github.com/TimothyEBaldwin/RISC_OS_Linux_Binary.git
 
 cd RISC_OS_Linux_Binary
@@ -6,7 +8,6 @@ patch -p1 < ../M1.patch
 make -j8 Built/qemu-arm
 make -j8 opengl
 
-RISCOSIMG=./Unix/RISCOS.IMG
 
 mkdir -p ./hostfs
 cp ../HardDisc4.util ./hostfs/HardDisc4.util,ffc
@@ -14,6 +15,8 @@ RISC_OS_IXFS_HardDisc4='IXFS:$' RISC_OS_Alias_IXFSBoot="run HardDisc4/util" ./op
 
 echo >> hostfs/HardDisc4/\!Boot/RO520Hook/Boot/Desktop
 echo "AddTinyDir HardDisc4" >> hostfs/HardDisc4/\!Boot/RO520Hook/Boot/Desktop
-
+cd ..
+fi
 # boot RISC OS
+cd RISC_OS_Linux_Binary
 RISC_OS_IXFS_HardDisc4='IXFS:$.HardDisc4' RISC_OS_Alias_IXFSBoot='HardDisc4.!Boot' ./opengl bwrap --bind ./hostfs / --ro-bind /usr /usr --ro-bind /lib /lib --ro-bind ./Built/qemu-arm /qemu-arm --ro-bind $RISCOSIMG /RISCOS.IMG --proc /proc --chdir / ./qemu-arm RISCOS.IMG >/dev/null
--- install_RPi5.sh
+++ install_RPi5.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+RISCOSIMG=./Unix/RISCOS.IMG
+if [[ ! -d RISC_OS_Linux_Binary ]] then
 git clone https://github.com/TimothyEBaldwin/RISC_OS_Linux_Binary.git
 
 cd RISC_OS_Linux_Binary
 patch -p1 < ../M1.patch
 make -j8 opengl
-
-RISCOSIMG=./Unix/RISCOS.IMG
 
 mkdir -p ./hostfs
 cp ../HardDisc4.util ./hostfs/HardDisc4.util,ffc
@@ -13,6 +13,8 @@ RISC_OS_IXFS_HardDisc4='IXFS:$' RISC_OS_Alias_IXFSBoot="run HardDisc4/util" ./op
 
 echo >> hostfs/HardDisc4/\!Boot/RO520Hook/Boot/Desktop
 echo "AddTinyDir HardDisc4" >> hostfs/HardDisc4/\!Boot/RO520Hook/Boot/Desktop
-
+cd ..
+fi
 # boot RISC OS
+cd RISC_OS_Linux_Binary
 RISC_OS_IXFS_HardDisc4='IXFS:$.HardDisc4' RISC_OS_Alias_IXFSBoot='HardDisc4.!Boot' ./opengl bwrap --bind ./hostfs / --ro-bind /usr /usr --ro-bind /lib /lib --ro-bind $RISCOSIMG /RISCOS.IMG --proc /proc --chdir / ./RISCOS.IMG >/dev/null
